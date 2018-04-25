d7e370c50ab8e018e4a8faa1a7c6e49d9cd7930c
George Phillips <gphillips8frw@gmail.com>
2018-04-25 20:55:09 +0100

    test fixed

--- test/test_coling_arg.py
+++ test/test_coling_arg.py
@@ -2,14 +2,17 @@ from coling_arg import DataPhaser
 
 test_func = DataPhaser(train_path='agr_en_train.csv', dev_path='agr_en_train.csv', col_names=['Code', 'Body', 'Tag'])
 
-def test():
-    X_train, X_val, Y_train, Y_val = test_func.load_files()
+
+def test_load_files():
+    data_phaser = DataPhaser(train_path='agr_en_train.csv', dev_path='agr_en_dev.csv',
+                             col_names=['Code', 'Body', 'Tag'])
+    X_train, X_val, Y_train, Y_val = data_phaser.load_files()
 
     assert 'The quality of re made now makes me think it is something to be bought from fish market' in X_val[0]
     assert 'Well said sonu..you have courage to stand against dadagiri of Muslims' in X_train[0]
 
-    assert Y_train == 'OAG'
-    assert Y_val == 'CAG'
+    assert Y_train[0] == 'OAG'
+    assert Y_val[0] == 'CAG'
 
     assert len(X_val) == 3001
     assert len(Y_val) == 3001
