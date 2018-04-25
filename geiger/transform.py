9eedf05caaedb860d08a292c4fee90c77125b511
Thiago Galery <tgalery@gmail.com>
2018-04-25 23:37:59 +0100

    adding embedding matrix generation

--- geiger/transform.py
+++ geiger/transform.py
@@ -1,6 +1,7 @@
 """
 Module for transforming sequences of text into Network Inputs
 """
+import numpy as np
 from keras.preprocessing import text, sequence
 
 
@@ -18,6 +19,7 @@ class KerasTransformer:
             max_seq_len: int: maximum individual sequence size.
         """
 
+        self.max_features = max_features
         self.max_seq_len = max_seq_len
         self.tokenizer = text.Tokenizer(num_words=max_features)
         self.build_vocabulary(x_texts)
@@ -46,3 +48,28 @@ class KerasTransformer:
         if pad and self.max_seq_len > 0:
             return sequence.pad_sequences(text_seqs, maxlen=self.max_seq_len)
         return text_seqs
+
+    def generate_embedding_matrix(self, embedding_lookup, embedding_size):
+        """
+        Generate an embedding matrix [vocab_size, n_dimensions]
+
+        Args:
+            embedding_lookup: dict: like interface that implements a .get method
+            embedding_size: int: dimensionality of embedding
+
+        Returns: np.array
+        """
+        nb_words = min(self.max_features, len(self.tokenizer.word_index))
+        embedding_matrix = np.zeros((nb_words, embedding_size))
+        for word, i in self.tokenizer.word_index.items():
+            if i >= nb_words:
+                return embedding_matrix
+            else:
+                embedding_vector = embedding_lookup.get(word)
+                if embedding_vector is not None:
+                    # TODO, we need to feature augumentation here if we are feeding this to the neuronet
+                    embedding_matrix[i] = embedding_vector
+                else:
+                    # Todo we are setting the vectors of <UNK> as zeros, maybe there's a better way
+                    continue
+        return embedding_matrix
--- test/test_transform.py
+++ test/test_transform.py
@@ -5,6 +5,14 @@ def test_text_transformation():
     # Expectations
     max_seq_len = 100
     max_features = 10000
+    embedding_lookup = {
+        "ramu": [0.2, 0.4],
+        "raj": [0.3, 0.5],
+        "play": [0.4, 0.6],
+        "basketball": [0.7, 0.8]
+    }
+    embedding_size = 2
+
     texts = [
         "We catch the car every day.",
         "It rains every afternoon.",
@@ -22,6 +30,9 @@ def test_text_transformation():
     ]
     transformer = transform.KerasTransformer(texts, max_features, max_seq_len)
     text_seq = transformer.texts_to_seq(["Raj drives her mother to New York", "Ramu plays basketball every summer"])
+    embed_matrix = transformer.generate_embedding_matrix(embedding_lookup, embedding_size)
+    mask = embed_matrix > 0
     assert len(text_seq) == 2, "We assume only 2 texts are processed."
     assert [t for t in text_seq[0] if t > 0] == [9, 28, 29, 26, 5, 6], "Vocab items for tokens in first text"
     assert [t for t in text_seq[1] if t > 0] == [2, 48, 1, 27], "Vocab items for tokens in second text"
+    assert set(embed_matrix[mask]) == set([0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8])
