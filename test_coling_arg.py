3825ed8994b5816ba6f0fa99834857429e90c6dc
George Phillips <gphillips8frw@gmail.com>
2018-04-25 20:18:24 +0100

    Update test_coling_arg

--- test_coling_arg.py
+++ test_coling_arg.py
@@ -1,24 +1,17 @@
-import pandas as pd
-import coling_arg
+from coling_arg import DataPhaser
 
+test_func = DataPhaser(train_path='agr_en_train.csv', dev_path='agr_en_train.csv', col_names=['Code', 'Body', 'Tag'])
 
-corp_dir = 'datasets/'
-train_path = 'agr_en_train.csv'
-dev_path = 'arg_en_dev.csv'
-col_names = ['Code', 'Body', 'Tag']
-test_func = coling_arg.DataPhaser(train_path=train_path, dev_path=dev_path, col_names=col_names)
+def test():
+    X_train, X_val, Y_train, Y_val = test_func.load_files()
 
+    assert 'The quality of re made now makes me think it is something to be bought from fish market' in X_val[0]
+    assert 'Well said sonu..you have courage to stand against dadagiri of Muslims' in X_train[0]
 
-def load_files():
-    train = pd.read_csv(corp_dir + train_path, names=col_names)
-    dev = pd.read_csv(corp_dir + train_path, names=col_names)
-    return train[col_names[1]], dev[col_names[1]], train[col_names[2]], dev[col_names[2]]
+    assert Y_train == 'OAG'
+    assert Y_val == 'CAG'
 
-
-X_train, X_val, Y_train, Y_val = test_func.load_files()
-test_X_train, test_X_val, test_Y_train, test_Y_val = load_files()
-
-assert X_train == test_X_train
-assert X_val == test_X_val
-assert Y_val == test_Y_val
-assert Y_train == test_Y_train
+    assert len(X_val) == 3001
+    assert len(Y_val) == 3001
+    assert len(X_train) == 11999
+    assert len(Y_train) == 11999
