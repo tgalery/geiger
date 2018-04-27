a6d342432c7bf8abf92cd06c1225412a6577d0f5
Efstathios Charitos <stathis.charitos@idioplatform.com>
2018-04-27 16:55:53 +0100

    lemmas and emojis

--- geiger/transform.py
+++ geiger/transform.py
@@ -3,7 +3,17 @@ Module for transforming sequences of text into Network Inputs
 """
 import numpy as np
 from keras.preprocessing import text, sequence
+import tweetokenize as tt
+from geiger.emoji import is_emoji, get_emoji_description
+from textblob import Word
+from tqdm import tqdm
 
+tokenizer = tt.Tokenizer()
+def fix_repeated_token (word):
+    if len(set(word)) == 1:
+        return word[0]
+    else:
+        return word
 
 class KerasTransformer:
     """
@@ -38,7 +48,9 @@ class KerasTransformer:
 
     @staticmethod
     def preprocess_text(text):
-        return text.replace('“', ' " ').replace("'", " ' ")
+        tokens = tokenizer.tokenize(text)
+        tokens = [ fix_repeated_token(token) for token in tokens ]
+        return ' '.join(tokens)
 
     def texts_to_seq(self, texts, pad=True):
         """
@@ -54,6 +66,14 @@ class KerasTransformer:
             return sequence.pad_sequences(text_seqs, maxlen=self.max_seq_len)
         return text_seqs
 
+    def handle_emoji(self, word, embedding_lookup):
+        description = get_emoji_description(word)
+        embedding_vectors = [
+            embedding_lookup.get(token)
+            for token in tokenizer.tokenize(description)]
+        embedding_vectors = [ x for x in embedding_vectors if x is not None ]
+        return np.mean(embedding_vectors, axis = 0)
+
     def generate_embedding_matrix(self, embedding_lookup, embedding_size):
         """
         Generate an embedding matrix [vocab_size, n_dimensions]
@@ -64,20 +84,35 @@ class KerasTransformer:
 
         Returns: np.array
         """
-
+        unhandled = []
         embedding_matrix = np.zeros((self.rel_features, embedding_size))
-        for word, i in self.tokenizer.word_index.items():
-            # need to correct as word_index[0] is reserved
-            if i >= self.max_features:
+        for word, i in tqdm(self.tokenizer.word_index.items()):
+            if i >= self.rel_features:
                 return embedding_matrix
             else:
                 lang = "en"
                 embedding_vector = embedding_lookup.get_vector(word, lang)
+
+                if embedding_vector is None and is_emoji(word):
+                    embedding_vector = self.handle_emoji(word, embedding_lookup)
+
+                # Try lemmatization
+                if embedding_vector is None:
+                    word = Word(word).lemmatize()
+                    embedding_vector = embedding_lookup.get(word)
+
+                # Try spellcheck
+                # if embedding_vector is None:
+                #     word = Word(word).correct()
+                #     embedding_vector = embedding_lookup.get(word)
+
                 if embedding_vector is not None:
                     # TODO, we need to feature augumentation here if we are feeding this to the neuronet
                     embedding_matrix[i] = embedding_vector
                 else:
+                    # print("Word {} is out of vocabulary.".format(word))
+                    unhandled.append(word)
                     # Todo we are setting the vectors of <UNK> as zeros, maybe there's a better way
-                    print("Word {} is out of vocabulary.".format(word))
                     continue
+        print("{} words were out of vocabulary.".format(len(unhandled)))
         return embedding_matrix
