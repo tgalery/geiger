4cec9b853451c694eea8cfe5937a21ec7ca8999a
Thiago Galery <tgalery@gmail.com>
2018-04-27 21:29:40 +0100

    utilities testing

---
+++ tests/test_utils.py
@@ -0,0 +1,18 @@
+from geiger import utils
+
+
+def test_is_devanagari():
+    lang = utils.is_devanagari("बिक्रम मेरो नाम हो")
+    assert lang is True
+
+
+def test_find_nrams():
+    n_grams = utils.find_ngrams("ABC", 2)
+    assert set(n_grams) == {'AB', 'BC'}
+
+
+def test_generate_ngrams():
+    pseudo_word = "porkistan"
+    ngrams = utils.generate_n_grams(pseudo_word)
+    print(ngrams)
+    assert set(ngrams) == {'por', 'ork', 'rki', 'kis', 'ist', 'sta', 'tan'}
