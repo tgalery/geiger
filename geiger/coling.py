a8ef532ab04b13228d1ca7dd4dfbec2d7391e7d7
Thiago Galery <tgalery@gmail.com>
2018-04-27 17:01:01 +0100

    adding missing files

---
+++ geiger/coling.py
@@ -0,0 +1,27 @@
+# possible values are {'OAG', 'CAG', 'NAG'}
+COLING_CLASSES = ['NAG', 'CAG', 'OAG']
+
+
+def one_hot_encode(tag):
+    """
+    Transform a class tag
+    Args:
+        tag: str: corresponding to the class tag
+
+    Returns:
+        list
+    """
+    return [1 if tag == class_tag else 0 for class_tag in COLING_CLASSES]
+
+
+def one_hot_decode(class_array):
+    """
+    Decode an array of ints representing the tweet class
+    Args:
+        class_array:
+
+    Returns:
+
+    """
+    class_idx = class_array.index(1)
+    return COLING_CLASSES[class_idx]
---
+++ geiger/stores.py
@@ -0,0 +1,48 @@
+from os import path
+
+from geiger.utils import load_word_vectors
+
+REPO_ROOT = path.abspath(__file__).rsplit("/geiger/", 1)[0]
+
+
+class MultiLangVectorStore:
+
+    vector_locs = {
+        "en": path.join(REPO_ROOT, "resources/wiki-news-300d-1M-subword.vec"),
+        "hi": path.join(REPO_ROOT, "resources/wiki.hi.vec"),
+    }
+
+    transform_locs = {
+        "en": path.join(REPO_ROOT, "geiger/libs/fastText_multilingual/alignment_matrices/en.txt"),
+        "hi": path.join(REPO_ROOT, "geiger/libs/fastText_multilingual/alignment_matrices/hi.txt"),
+    }
+
+    supported_langs = ["en", "hi"]
+
+    vector_stores = dict()
+
+    def __init__(self):
+
+        for lang in self.supported_langs:
+            vec_path = self.vector_locs[lang]
+            transform_path = self.transform_locs[lang]
+            self.vector_stores[lang] = load_word_vectors(vec_path, transform_fpath=transform_path)
+
+    def get_vector(self, word, lang):
+        if lang not in self.supported_langs:
+            raise ValueError("No support for language {}".format(lang))
+        try:
+            return self.vector_stores[lang][word]
+        except KeyError:
+            return None
+
+    def get_vectors(self, words, lang):
+        vecs = []
+        for word in words:
+            try:
+                word_vec = self.get_vector(word, lang)
+                if word_vec:
+                    vecs.append(word_vec)
+            except ValueError:
+                print("Can't get words for lang {}".format(lang))
+        return vecs
